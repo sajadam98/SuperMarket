cbf485499e19125a9de2e6fb5d46f4bf943d3027
unknown <Sajad Amiri@PC121.TaavSys.local>
2022-05-01 18:38:47 +0430

    initial product and category entities

---
+++ SuperMarket.Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ProductKey { get; set; }
+    public int Price { get; set; }
+    public string Brand { get; set; }
+    public int MinimumAllowableStock { get; set; }
+    public int MaximumAllowableStock { get; set; }
+    public int Stock { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+}
