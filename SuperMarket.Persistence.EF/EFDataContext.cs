0de7386096b928e64baf5d310dbb28143a78179c
unknown <Sajad Amiri@PC121.TaavSys.local>
2022-05-02 08:23:48 +0430

    initial EFDataContext & EFUnitOfWork

---
+++ SuperMarket.Persistence.EF/EFDataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class EFDataContext : DbContext
+{
+    public EFDataContext(string connectionString) :
+        this(new DbContextOptionsBuilder().UseSqlServer(connectionString).Options)
+    {
+    }
+
+    public EFDataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductEntityMap).Assembly);
+    }
+}
---
+++ SuperMarket.Persistence.EF/EFUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿public class EFUnitOfWork : UnitOfWork
+{
+    private readonly EFDataContext _dbContext;
+
+    public EFUnitOfWork(EFDataContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void Save()
+    {
+        _dbContext.SaveChanges();
+    }
+}
